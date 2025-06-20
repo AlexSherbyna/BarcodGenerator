3aa0ae5782100f2bde5fd7ce7fcdba81a6cab250
OleksandrSherbyna <alex.sherbyna1@gmail.com>
2025-06-20 21:23:02 +0300

    changed start number for generator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> barcodeList = ListNumbersService.createSerialNumbersList("fg",999,3);
+        ArrayList<String> barcodeList = ListNumbersService.createSerialNumbersList("fg",9,3);
         WriteSerialNumberToTextFile.writeSerialNumberToTextFile(barcodeList);
         BarcodeService.createSerialNumberToPDF(barcodeList);
     }
